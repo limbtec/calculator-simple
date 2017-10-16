cbf0289ee3074c95ec2f8c08a67137fcb3fddb0f
Andy Smith <andy.smith.gfx@gmail.comandy.smith.gfx@gmail.comandy.smith.gfx@gmail.comandy.smith.gfx@gmail.com>
2017-10-16 14:06:22 +0100

    Calculator Boolean

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,32 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
     
+    var addition = false
+    
     
     
     @IBAction func addButton(_ sender: Any) {
-    
-        print(text1.text!)
-        print(text2.text!)
-        answerLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
-
+        
+        if addition {
+            answerLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+            
+        } else {
+            answerLabel.text = "\(Double(text1.text!)! - Double(text2.text!)!)"
+            
+        }
+        
+        
+        
+        
+        
+        
     }
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
